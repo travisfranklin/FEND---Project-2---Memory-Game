ef8cf36a3b2f74894f66982c4507355cb13170b1
travisfranklin <travislf88@gmail.com>
2019-07-08 14:42:21 -0500

    adjusted queries across scripts.js

--- scripts.js
+++ scripts.js
@@ -18,9 +18,7 @@
 ///////////////////////////////////////
 
 // Variables
-function query(x) {
-  return document.querySelector(x);
-};
+
 const arrayCards = [
   "bell-slash",
   "bell-slash",
@@ -39,17 +37,72 @@ const arrayCards = [
   "gem",
   "gem"
 ];
-let queryDeck = query(".deck"),
-  queryMoves = query(".moves"),
-  queryMovesLabel = query(".moves-label"),
-  queryReset = query(".fa-repeat"),
-  queryTimer = query(".timer"),
-  queryTimerLabel = query(".timer-label"),
-  flippedCards = [],
+const queryDeck = document.getElementById("deck");
+const queryMoves = document.getElementById("moves");
+const queryMovesLabel = document.getElementById("moves-label");
+const queryReset = document.getElementsByClassName("fa-repeat");
+const queryTimer = document.getElementById("timer");
+const queryTimerLabel = document.getElementById("timer-label");
+  
+let flippedCards = [],
   movesCount = 0,
   paused = false,
   started = false,
   timeCount = 0;
+
+// Call functions to enable game functionality, establish global variables
+function init() {
+  // Cards need to exist before their classes can be selected
+  createCards();
+
+  // Variables for selectors that can only be called after squares are created
+  let queryCard = document.querySelectorAll(".card");
+
+  // Enable functionality for square interaction
+  // document.getElementsByClassName("card").addEventListener( "click" , flipCard);
+  // let cardClass = document.getElementsByClassName("card");
+  // Array.from(cardClass).forEach(function(element) {
+  //   element.addEventListener( "click" , function (event) {
+  //     console.log("CLICK");
+  //   });
+  // });
+
+  document.body.addEventListener('click', function (evt) {
+    if (evt.target.classList.contains('card')) {
+      flipCard()
+    }
+  }, false);
+
+  // Shuffle the icons, then create li items for each
+  function createCards() {
+    let cards = shuffle(arrayCards);
+    for (let card of cards) {
+      const li = document.createElement("li");
+      const i = document.createElement("i");
+      li.setAttribute("class", "card ");
+      i.setAttribute("class", "fas fa-" + card);
+      li.appendChild(i);
+      queryDeck.appendChild(li);
+    }
+  }
+
+  // Function to shuffle deck items
+  function shuffle(array) {
+    var currentIndex = arrayCards.length,
+      temporaryValue,
+      randomIndex;
+
+    while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  }
+}
 
 // Run after the page has finished loading
 document.addEventListener("DOMContentLoaded", function() {
@@ -57,54 +110,11 @@ document.addEventListener("DOMContentLoaded", function() {
   init();
 
   // Reset the game when button is clicked
-  query(".reset").addEventListener( "click" , resetGameWarning);
-
+  const resetListener = document.querySelectorAll(".reset");
+  resetListener[0].addEventListener( "click" , resetGameWarning);
   ///////////////////////////////////////
   //  1b. Start
   ///////////////////////////////////////
-
-  // Call functions to enable game functionality, establish global variables
-  function init() {
-    // Cards need to exist before their classes can be selected
-    createCards();
-
-    // Variables for selectors that can only be called after squares are created
-    let queryCard = query(".card");
-
-    // Enable functionality for square interaction
-    // hoverCard();
-    queryCard.addEventListener( "click" , flipCards);
-
-    // Shuffle the icons, then create li items for each
-    function createCards() {
-      let cards = shuffle(arrayCards);
-      for (let card of cards) {
-        const li = document.createElement("li");
-        const i = document.createElement("i");
-        li.setAttribute("class", "card ");
-        i.setAttribute("class", "fas fa-" + card);
-        li.appendChild(i);
-        queryDeck.appendChild(li);
-      }
-    }
-
-    // Function to shuffle deck items
-    function shuffle(array) {
-      var currentIndex = arrayCards.length,
-        temporaryValue,
-        randomIndex;
-
-      while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-      }
-
-      return array;
-    }
-  }
 });
 
 ///////////////////////////////////////
@@ -113,12 +123,18 @@ document.addEventListener("DOMContentLoaded", function() {
 
 // Reset game to initialization state
 function resetGame() {
-  deck.empty();
-  movesCount = -1;
+  // while(queryDeck[0].firstChild) { 
+  //   queryDeck[0].removeChild(queryDeck[0].firstChild);  
+  //   movesCount = -1;
+  //   if (queryDeck[0].firstChild == null) {
+  //       break;
+  //   }
+  // }
+  queryDeck.firstChild.innerHTML = '';
   moveCounter();
   paused = false;
   timerLabel.html("Seconds");
-  query(".far.fa-star").toggleClass("fas far");
+  document.getElementsByClassName("far fa-star").toggleClass("fas far");
   arrIconsOpen = [];
   resetTimer();
   init();
@@ -127,17 +143,11 @@ function resetGame() {
 // Modal dialog to confirm reset
 function resetGameWarning() {
   paused = true;
-
-  confirm({
-    message: "are you sure you wish to reset?",
-    callback: function(value) {
-      if (value) {
-        resetGame();
-      } else {
-        paused = false;
-      }
-    }
-  });
+  if (confirm("are you sure you wish to reset?") == true) {
+    resetGame();
+  } else {
+    paused = false
+  }
 }
 
 ///////////////////////////////////////
@@ -150,14 +160,14 @@ function checkMatch() {
   if (flippedCards[0] === flippedCards[1]) {
     queryDeck
       .find("open")
-      .removeClass("open show")
-      .addClass("match flipped");
+      .classList.remove("open show")
+      .classList.add("match flipped");
   } else {
     setTimeout(function() {
       queryDeck
         .find("open")
-        .addClass("wrong")
-        .removeClass("open show");
+        .classList.add("wrong")
+        .classList.remove("open show");
     });
   }
 
@@ -165,7 +175,7 @@ function checkMatch() {
   movesCount++;
 
   // If all squares have been matched, win the game
-  if (query(".match").length === arrayCards.length) {
+  if (document.getElementsByClassName("match").length === arrayCards.length) {
     winGame();
   }
 
@@ -180,37 +190,39 @@ function checkMatch() {
 // When a square is clicked, do this stuff
 function flipCard() {
   // Define currently clicked square
-  let queryThis = query(this);
+  let queryThis = document.querySelectorAll(this);
 
   // Traverse DOM element for this square's icon class
   let queryThisClass = queryThis[0].firstChild.classList[1];
 
+  console.log(queryThis);
+
   // Prevent flipping over more than two cards at once
-  if (
-    queryThis.hasClass("open") ||
-    queryThis.hasClass("match") ||
-    query(".open").length === 2
-  ) {
-    return;
-  }
+  // if (
+  //   queryThis.hasClass("open") ||
+  //   queryThis.hasClass("match") ||
+  //   document.getElementsByClassName("open").length === 2
+  // ) {
+  //   return;
+  // }
 
   // Start timer when first square is clicked
-  if (started === false) {
-    startTimer();
-  }
+  // if (started === false) {
+  //   startTimer();
+  // }
 
   // If the temporary array doesn't yet have two items in it
-  if (arrayCards.length > 2) {
-    queryThis.addClass("open show flipped");
+  // if (arrayCards.length < 2) {
+  //   queryThis.addClass("open show flipped");
 
-    // Add class of clicked square to the temp array
-    flippedCards.push(queryThisClass);
-  }
+  //   // Add class of clicked square to the temp array
+  //   flippedCards.push(queryThisClass);
+  // }
 
   // If two squares have been flipped, check to see if they match
-  if (flippedCards.length === 2) {
-    checkMatch();
-  }
+  // if (flippedCards.length === 2) {
+  //   checkMatch();
+  // }
 }
 
 ///////////////////////////////////////
@@ -219,11 +231,11 @@ function flipCard() {
 
 // Change solid star icon class to outlined version when a star is removed
 function removeStar() {
-  let queryStar = query(".fas .fa-star:last");
-  queryStar.toggleClass("fas far");
+  let queryStar = document.querySelectorAll(".fas .fa-star:last");
+  queryStar[0].toggleClass("fas far");
 
   // Get current star count
-  let starCount = query(".fas.fa-star").length;
+  let starCount = document.querySelectorAll(".fas.fa-star").length;
 }
 
 // Increment moves
@@ -233,9 +245,9 @@ function moveCounter() {
 
   // Grammar check so score doesn't read "1 Moves"
   if (movesCount === 1) {
-    queryMovesLabel.html("Move");
+    queryMovesLabel.textContent = "Move";
   } else {
-    queryMovesLabel.html("Moves");
+    queryMovesLabel.textContent = "Moves";
   }
 
   // Star removal when specific move counts are hit
@@ -249,7 +261,7 @@ function moveCounter() {
   }
 
   // Update page display of move count
-  queryMoves.html(movesCount);
+  queryMoves.textContent = movesCount;
 }
 
 ///////////////////////////////////////
@@ -279,4 +291,4 @@ function moveCounter() {
 // Get star count
 
 // Announcement and option to reset
-// message: 'You win with ' + (3 - document.querySelector('.star').length) + ' or ' + starCount + ' stars',
+// message: 'You win with ' + (3 - document.querySelectorAll('.star').length) + ' or ' + starCount + ' stars',
